110ed42add045b1ecee913468bfb5e001e625390
amesclir <amesclir@gmail.com>
2024-01-31 14:38:44 +0100

    minor changes

--- PCM_S3_S1.R
+++ PCM_S3_S1.R
@@ -16,15 +16,17 @@ mtext("ln(rate)",side=1,line=4,cex=0.9)
 
 ## fit BM model
 fitBM_GC<-fitContinuous(bacteria.tree,GC,model="BM")
+##fit lambda model
+fitlambda_GC<-fitContinuous(bacteria.tree,GC,model="lambda",bounds=list(lambda=c(0,1.005)))
 ## fit OU model
 fitOU_GC<-fitContinuous(bacteria.tree,GC,model="OU",bounds=list(alpha=c(0,100)))
 ## accumulate AIC scores in a vector
-aic_GC<-setNames(c(AIC(fitBM_GC),AIC(fitOU_GC)),c("BM","OU"))
+aic_GC<-setNames(c(AIC(fitBM_GC),AIC(fitlambda_GC),AIC(fitOU_GC)),c("BM","lambda", "OU"))
 ## compute and print Akaike weights
 aic.w(aic_GC)
 
 #Best model
-fitOU_GC
+fitlambda_GC
 
 ##phylogenetic signal
 fitBMlambda_GC<-phylosig(bacteria.tree,GC,method="lambda", test=T)
